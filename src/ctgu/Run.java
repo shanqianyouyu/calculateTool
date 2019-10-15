bcfd3043ea34292c34a4eb2ea47bb995731086b7
sc <1131497375@qq.com>
2019-10-15 16:33:19 +0800

    修改主启动类 com.Run.java

---
+++ src/ctgu/Run.java
@@ -0,0 +1,23 @@
+package ctgu;
+
+import ctgu.awt.frame.load.Load;
+import ctgu.awt.frame.login.MainFrame;
+
+/**
+ * Copyright © 2019 eSunny Info. Tech Ltd. All rights reserved.
+ * 
+ * 功能描述：
+ * 
+ * @Package: ctgu
+ * @author: 拉布拉多
+ * @date: 2019年10月15日 下午4:26:08
+ */
+
+public class Run {
+
+	public static void main(String[] args) {
+		Load.main(args);
+
+	}
+
+}
